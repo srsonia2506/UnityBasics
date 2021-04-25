65ab91c38c17b8d047ce0218e5b375b9b9a2ca25
Saravanan Sonia <srsonia98@yahoo.com.sg>
2021-04-25 16:31:47 +0800

    Text Effect

---
+++ Mechanics/Text Effects/Assets/ActivateWriter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateWriter : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+
+    private void Awake()
+    {
+        messageText = transform.Find("Message").GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        textWriter.AddWriter(messageText, "Not sure what to type but a girl got to try... so here goes!!!" +
+            " Once upon a time, there lived a prince on Planet End.", 0.2f, true);
+    }
+
+    
+}
